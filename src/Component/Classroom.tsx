daf65632359424f36100cae8beefbad94b191771
Bhavya Bharti <bhavyab@outlook.com>
2020-07-24 02:09:34 -0700

    initial setup

--- src/Component/Classroom.tsx
+++ src/Component/Classroom.tsx
@@ -3,7 +3,9 @@ import '../styles/App.scss';
 
 export function Classroom() {
     return (
-        <div className="App">  This is my class </div>
+        <div className="App">  This is my class
+            <li>go to <a href="/">Home</a></li>
+        </div>
     );
 }
 
--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -3,7 +3,9 @@ import '../styles/App.scss';
 
 export function Home() {
     return (
-        <div className="App"> Home Sweet Home </div>
+        <div className="App"> Home Sweet Home 
+            <li>go to <a href="/classroom">classroom</a></li>
+        </div>
     );
 }
 
